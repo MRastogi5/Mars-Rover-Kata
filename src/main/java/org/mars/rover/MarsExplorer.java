bf0e06998cc02545ccf54ccbe07607098b015b92
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-02-15 23:09:24 +0000

    Added Rover moving functionality

--- src/main/java/org/mars/rover/MarsExplorer.java
+++ src/main/java/org/mars/rover/MarsExplorer.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class MarsExplorer {
 
+    Plateau plateau ;
+
     public static void main(String[] args) {
 
         String str;
-        int xRover=0, yRover=0;
-        char dirRover = 0;
-        String moves = "";
         int i = 0;
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter Plateau co-ordinates : ");
         int xPlateau = scanner.nextInt();
@@ -19,22 +20,73 @@ public class MarsExplorer {
 
         System.out.println("Please enter how many rover  : ");
         int roverCount = scanner.nextInt();
+        int[] xRover= new int[roverCount];
+        int[] yRover= new int[roverCount];
+        char[] dirRover = new char[roverCount];
+        String[] movingPattern = new String[roverCount];
         while (i < roverCount) {
             System.out.println("Please enter Rover "+i+" position co-ordinates : ");
-            xRover = scanner.nextInt();
-            yRover = scanner.nextInt();
-            if(xRover > xPlateau || yRover > yPlateau){
+            xRover[i] = scanner.nextInt();
+            yRover[i] = scanner.nextInt();
+            if(xRover[i] > xPlateau || yRover[i] > yPlateau){
                 System.out.println("Rover is outside of Plateau. Please enter correct co-ordinates: ");
-                xRover = scanner.nextInt();
-                yRover = scanner.nextInt();
+                xRover[i] = scanner.nextInt();
+                yRover[i] = scanner.nextInt();
             }
-            dirRover = scanner.next().charAt(0);
+            dirRover[i] = scanner.next().charAt(0);
             System.out.println("Please enter Rover moving pattern : ");
-            moves = scanner.nextLine();
+            movingPattern[i] = scanner.next();
+
+            if(!movingPattern[i].matches("[LRM]*")){
+                System.out.println("Please enter correct moving pattern : ");
+                movingPattern[i] = scanner.next();
+            }
             i++;
         }
         scanner.close();
 
+        Plateau plateau = createPlateau(xPlateau, yPlateau);
+        addRoverToPlateau(roverCount, xRover, yRover, dirRover, plateau);
+        i = 0;
+        while (i < roverCount) {
+            Rover rover = plateau.getRovers().get(i);
+            moveTheRover(movingPattern[i], rover);
+            System.out.println("Rover Final Position : " + rover.getPosition());
+            i++;
+        }
+        //;
+    }
+
+    private static void addRoverToPlateau(int roverCount, int[] xRover, int[] yRover, char[] dirRover, Plateau plateau) {
+        int i = 0;
+        while (i < roverCount) {
+            Rover rover = new Rover(xRover[i], yRover[i], dirRover[i] );
+            plateau.addRover(rover);
+            i++;
+        }
+    }
+
+    private static void moveTheRover(String movingPattern, Rover rover) {
+        char[] moveChar = movingPattern.toCharArray();
+        for(char c : moveChar){
+            switch (c){
+                case 'L' :
+                    rover.left();
+                    break;
+                case 'R':
+                    rover.right();
+                    break;
+                case 'M':
+                    rover.move();
+                    break;
+            }
+        }
+    }
+
+    private static Plateau createPlateau(int xPlateau, int yPlateau) {
+        Plateau plateau = Plateau.getInstance(xPlateau, yPlateau);
+        System.out.println("Plateau Created !!! ");
+        return plateau;
     }
 
 }
--- src/main/java/org/mars/rover/Plateau.java
+++ src/main/java/org/mars/rover/Plateau.java
@@ -1,9 +1,42 @@
 package org.mars.rover;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plateau {
+
+    public int getX_axis() {
+        return x_axis;
+    }
+
+    public void setX_axis(int x_axis) {
+        this.x_axis = x_axis;
+    }
+
+    public int getY_axis() {
+        return y_axis;
+    }
+
+    public void setY_axis(int y_axis) {
+        this.y_axis = y_axis;
+    }
 
     private int x_axis;
     private int y_axis;
+
+    public List<Rover> getRovers() {
+        return rovers;
+    }
+
+    public void setRovers(List<Rover> rovers) {
+        this.rovers = rovers;
+    }
+
+    public void addRover(Rover rover) {
+        rovers.add(rover);
+    }
+
+    private List<Rover> rovers = new ArrayList<>();
 
     private static Plateau plateau;
     private Plateau(int x, int y) {
